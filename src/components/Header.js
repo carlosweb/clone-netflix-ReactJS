53e982f3c6ec1d4ebb9854454eb10f3f8771b01b
Carlos Eduardo <kaduinfo@gmail.com>
2020-10-01 22:15:26 -0300

    Link header atualizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ export default ({black}) => {
     return (
         <header className={black ? 'black' : ''}>
            <div className="header--logo">
-               <a href="#">
+               <a href="/">
                    <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-2-1.png" />
                </a>
            </div>
            <div className="header--user">
-               <a href="#">
+               <a href="/">
                    <img src="https://img.pngio.com/customer-login-avatar-head-portrait-client-user-png-transparent-png-avatar-600_500.png" />
                </a>
            </div>
